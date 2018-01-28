abfa8273fd6d245ce4820962e63985b19995ba4d
gafepa92 <33100821+gafepa92@users.noreply.github.com>
2018-01-28 19:22:18 +0100

    Update circle_detector.cpp

--- src/circle_detector.cpp
+++ src/circle_detector.cpp
@@ -19,15 +19,6 @@
 #include <vector>
 #include <stdio.h>
 
-/*//constants
-const int GAUSSIAN_BLUR_SIZE = 7;
-const double GAUSSIAN_BLUR_SIGMA = 2;
-const double CANNY_EDGE_TH = 150;
-const double HOUGH_ACCUM_RESOLUTION = 2;
-const double MIN_CIRCLE_DIST = 40;
-const double HOUGH_ACCUM_TH = 70;
-const int MIN_RADIUS = 20;
-const int MAX_RADIUS = 80;*/
 
 /** Global variables */
 /*download face detector file from build folder*/
@@ -47,8 +38,7 @@ int main(int argc, char *argv[])
 	  int cam_id; //camera id . Associated to device number in /dev/videoX
     std::vector<cv::Rect> faces; //vector in which data abaout the faces will be saved
     cv::Mat gray_image;// gray image for detecting data
-    /*std::vector<cv::Vec3f> circles;
-    cv::Point center;*/
+  
     int radius;
     //download the file from build folder in our repository. if file not found the code is over
     if( !face_cascade.load( face_cascade_name ) ){ printf("--(!)Error loading face cascade\n"); return -1; };
@@ -88,30 +78,22 @@ int main(int argc, char *argv[])
         }
     //**************** Find circles in the image ****************************
 
-        //clear previous circles
-        //circles.clear();
+        
 
         // If input image is RGB, convert it to gray
         cv::cvtColor(image, gray_image, CV_BGR2GRAY);
         cv::equalizeHist(gray_image,gray_image);
-        //Reduce the noise so we avoid false circle detection filter
-        //cv::GaussianBlur( gray_image, gray_image, cv::Size(GAUSSIAN_BLUR_SIZE, GAUSSIAN_BLUR_SIZE), GAUSSIAN_BLUR_SIGMA );
+        
         //-- Detect faces
         face_cascade.detectMultiScale(gray_image, faces, 1.1, 2, 0|CV_HAAR_SCALE_IMAGE, cv::Size(30, 30) ); //CV_HAAR_SCALE_IMAGE CASCADE_SCALE_IMAGE
         //Apply the Hough Transform to find the circles
-	      // third parameter is used to select the Hough method HOUGH_STANDARD,HOUGH_PROBABILISTIC, 		HOUGH_MULTI_SCALE & HOUGH_GRADIENT
-        //cv::HoughCircles( gray_image, circles, CV_HOUGH_GRADIENT, HOUGH_ACCUM_RESOLUTION, MIN_CIRCLE_DIST, CANNY_EDGE_TH, HOUGH_ACCUM_TH, MIN_RADIUS, MAX_RADIUS );
-
+	      
         //draw circles on the image
         for(unsigned int ii = 0; ii < faces.size(); ii++ )
         {
           cv::Point center(faces[ii].x + faces[ii].width/2, faces[ii].y + faces[ii].height/2);
           cv::ellipse(image, center, cv::Size(faces[ii].width/2, faces[ii].height/2), 0, 0, 360, cv::Scalar(255, 0, 255), 4, 8, 0);
-          //cv::Mat faceROI = gray_image(faces[i]);
-            /*        cv::circle(image, center, 5, cv::Scalar(0,0,255), -1, 8, 0 );// circle center in green
-                    cv::circle(image, center, radius, cv::Scalar(0,255,0), -1, 8, 0 );
-		    // *///circle perimeter in red (0,0,x) green (0,x,0) blue (x,0,0)
-        }
+         }
 
     //********************************************************************
 
